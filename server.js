4940fffe265c74414250cb10b7e8fbf2f97e2096
Kichan <rlgusrlcks00@gmail.com>
2023-07-23 19:58:17 +0900

    study

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.listen(8080, function(){
+    console.log('listening on 8080')
+});
+
+app.get('/pet', function(요청, 응답){
+    응답.send('pet shoping site');
+});
+
+app.get('/beauty', function(req,res){
+    res.send('beauty shoping site');
+});
