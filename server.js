91aa9c41870fa5b615924d61a55900309c6753f5
Kichan <rlgusrlcks00@gmail.com>
2023-07-23 21:15:18 +0900

    study

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({extended : true}));
 
 app.listen(8080, function(){
     console.log('listening on 8080')
@@ -11,4 +13,21 @@ app.get('/pet', function(요청, 응답){
 
 app.get('/beauty', function(req,res){
     res.send('beauty shoping site');
+});
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/write',function(req,res){
+    res.sendFile(__dirname + '/write.html')
+});
+
+app.get('/write1',(req,res) => {res.sendFile(__dirname + '/write.html')});
+//신문법, 실무에서는 이렇게 쓰지 않을까?
+
+app.post('/add', function(req,res){
+    res.send('전송완료')
+    console.log(req.body.todo)
+    console.log(req.body.date)
 });
