40c5b10a34260ec1789ee99cbcfd76521f19f98e
Kichan <rlgusrlcks00@gmail.com>
2023-07-27 17:44:58 +0900

    study

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyparser = require('body-parser');
 app.use(bodyparser.urlencoded({extended : true}));
 const MongoClient = require('mongodb').MongoClient;
-
+app.set('view engine','ejs');
 
 // MongoClient.connect('mongodb+srv://admin:gg0069759@cluster0.cnufat6.mongodb.net/?retryWrites=true&w=majority',function(error, client){
 //     app.listen(8080, function(){
@@ -11,7 +11,7 @@ const MongoClient = require('mongodb').MongoClient;
 //     });
 
 // });
-MongoClient.connect('mongodb+srv://admin:<PW>@cluster0.cnufat6.mongodb.net/?retryWrites=true&w=majority',{ useUnifiedTopology: true }, function(error, client){
+MongoClient.connect('mongodb+srv://admin:poiu0987@cluster0.cnufat6.mongodb.net/?retryWrites=true&w=majority',{ useUnifiedTopology: true }, function(error, client){
     if (error) return console.log(error)
     
     db = client.db('todoapp');
@@ -51,10 +51,34 @@ app.get('/write1',(req,res) => {res.sendFile(__dirname + '/write.html')});
 
 app.post('/add', function(req,res){
     res.send('전송완료')
-    db.collection('post').insertOne({todo: req.body.todo, date: req.body.date}, function(error,res){
-        console.log('save data');
+    db.collection('counter').findOne({name : 'countPost'}, function(err,ret){
+        console.log(ret.totalPost);
+        var TotalPost = ret.totalPost;
+
+        db.collection('post').insertOne({ _id : TotalPost, todo: req.body.todo, date: req.body.date}, function(error,ret){
+            console.log('save data');
+
+            db.collection('counter').updateOne({name : 'countPost'},{ $inc : {totalPost : 1} },function(error,ret){
+                if(error){return console.log(error)};
+            });
+            if(error){return console.log(error)};
+        });
     });
+
     console.log(req.body.todo)
     console.log(req.body.date)
-
 });
+
+
+app.get('/list', function(req, res){
+
+    db.collection('post').find().toArray(function(err, ret){
+        console.log(ret.length)
+
+
+        console.log(ret);
+        res.render('list.ejs', {posts: ret});
+    });
+});
+
+//Part2 AJAX로 삭제 요청하기1
