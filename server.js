8ed548e66b79272d515ef533b4f9395c5ec8de22
Kichan <rlgusrlcks00@gmail.com>
2023-08-16 22:10:54 +0900

    study

--- server.js
+++ server.js
@@ -152,5 +152,38 @@ app.post('/welcome',function(req,res){
     console.log(req.body);
 });
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 
+app.use(session({secret: '비밀코드', resave: true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.post('/login', passport.authenticate('local', { 
+    failureRedirect : '/fail'
+}), function(req,res){
+    res.redirect('/')
+    passport.use(new LocalStrategy({
+        usernameField: 'id',
+        passwordField: 'pw',
+        session: true,
+        passReqToCallback: false,
+      }, function (입력한아이디, 입력한비번, done) {
+        //console.log(입력한아이디, 입력한비번);
+        db.collection('user').findOne({ email: 입력한아이디 }, function (에러, 결과) {
+          if (에러) return done(에러)
+      
+          if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+          if (입력한비번 == 결과.pw) {
+            return done(null, 결과)
+          } else {
+            return done(null, false, { message: '비번틀렸어요' })
+          }
+        })
+      }));
+});
+
+app.get('/login', function(req,res){
+    res.render(login.ejs)
+});
